c231d08061db92889ac49682abcb884bb9036302
C.Cuong <zennygood01@gmail.com>
2024-11-27 08:41:01 +0700

    BT cuoi ky

---
+++ BT_CuoiKy/app/src/main/java/com/example/cuoiky_bt/C_Item.java
@@ -0,0 +1,4 @@
+package com.example.cuoiky_bt;
+
+public class C_Item {
+}
--- BT_CuoiKy/app/src/main/java/com/example/cuoiky_bt/MainActivity.java
+++ BT_CuoiKy/app/src/main/java/com/example/cuoiky_bt/MainActivity.java
@@ -1,24 +1,40 @@
 package com.example.cuoiky_bt;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigation;
+    View fragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        fragment = findViewById(R.id.fragment);
+
+        bottomNavigation.setOnItemSelectedListener(item -> {
+            Fragment fragment = new fragment_featured();;
+            if(item.getItemId() == R.id.feat){
+                fragment = new fragment_featured();
+            } else if (item.getItemId() == R.id.feed) {
+                fragment = new fragment_feed();
+            }else if (item.getItemId() == R.id.settings) {
+                fragment = new fragment_setting();
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragment).commit();
+            return true;
         });
     }
 }
